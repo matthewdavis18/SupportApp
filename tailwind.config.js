abc4d0d7c4dc6a913f36cd01db08a52a0ddc2d11
Matthew Davis <matthewdavis@MacBook-Air.local>
2021-10-27 16:47:01 -0400

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}","./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     container: {
