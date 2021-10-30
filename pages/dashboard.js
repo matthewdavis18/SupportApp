9672e749375bfa96bac75633e151a5fba90e7b8d
Matthew Davis <matthewdavis@MacBook-Air.local>
2021-10-30 14:16:54 -0400

    api

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,18 +8,19 @@
 
          }
      }
-   const data = fetch('https://upside.zendesk.com/api/v2/search/count?query=created>2021-05-01 created<2021-05-31 custom_field_33985008:"offer__expired/declined"',init
+   const data = await fetch(`https://upside.zendesk.com/api/v2/search/count?query=created>2021-05-01 created<2021-05-31 custom_field_33985008:"offer__expired/declined"`,init
    );
+   const count = await JSON.stringify(data)
    return {
-     props: { data }, // will be passed to the page component as props
+     props: { count }, // will be passed to the page component as props
    };
  }
 
 
-function Dashboard() {
+function Dashboard(count) {
    
   
-  return <div>data.count</div>;
+  return <div>{count.count}</div>;
 }
 
 export default Dashboard 
